024ebbb0c2f87ea81f99e2b7201ebc0307735d08
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2024-09-17 10:58:52 -0700

    Update 06_Advanced_OOP.py

--- 06_Advanced_OOP.py
+++ 06_Advanced_OOP.py
@@ -563,3 +563,25 @@ class Wizard(User):
 wizard1 = Wizard('Merlin', 60, 'merlin@gmail.com')
 print(wizard1.email)
 
+-------------
+
+
+# introspection - the ability to determine the object at run time
+class User:
+    def __init__(self, email):
+        self.email = email
+
+    def sign_in(self):
+        print('Logged in')
+
+class Wizard(User):
+    def __init__(self, name, power, email):
+        super().__init__(email)        # reffering to Super Class (class above), no self needed
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f'Attacking with power of {self.power}')
+
+wizard1 = Wizard('Merlin', 60, 'merlin@gmail.com')
+print(dir(wizard1))
