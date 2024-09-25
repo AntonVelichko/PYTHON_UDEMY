a40d901cadf4debdd2eaff39b7809f7b74a3301e
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2024-09-24 22:54:51 -0700

    Update 04__Functions.py

--- 04__Functions.py
+++ 04__Functions.py
@@ -4,7 +4,7 @@ def say_hello():
 
 say_hello()                 # hello!
 print(say_hello)            # <function say_hello at 0x7f413b6461f0>
-print(say_hello())          # hello!   # None / returns 'None' since function doesn't have Return
+print(say_hello())          # hello!   # None --> returns 'None' since the function doesn't have a Return
 
 
 
@@ -22,7 +22,7 @@ say_hi('Anton', 'happy')                # hi Anton happy
 # 81 positional arguments
 say_hi('sad', 'Bibi')                   # hi sad Bibi
 
-# keyword argument (bad practice, complicate the code)
+# keyword argument (bad practice, complicates the code)
 say_hi(emoji = 'sad', name = 'Bibi')   # hi Bibi  sad
 
 
@@ -59,16 +59,16 @@ print(sum(10,sum(10,5)))                 # 25
 
 
 def sum(num1, num2):
-  def another_func(num1,num2):
+  def another_func(num1, num2):
     return num1 + num2
-  return another_func(num1,num2)
+  return another_func(num1, num2)
 
 total = sum(5, 10)
 print(total)                              # 15
 
 
 def sum(num1, num2):
-  def another_func(num1,num2):
+  def another_func(num1, num2):
     return num1 + num2
   return another_func
 
@@ -77,7 +77,7 @@ print(total)              # <function sum.<locals>.another_func at 0x7ffb7428fa6
 
 
 def sum(num1, num2):
-  def another_func(num1,num2):
+  def another_func(num1, num2):
     return num1 + num2
   return another_func
 
@@ -85,7 +85,7 @@ total = sum(5, 10)
 print(total(10, 20))                      # 30
 
 
-# after 'return' function stops execute code
+# after the 'return' function stops executing code
 
 
 
