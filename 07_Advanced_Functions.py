06ef98d3ba1cd0dc9aa017f94a3c3b72286e4b85
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2024-09-28 12:08:26 -0700

    Update 07__Advanced_Functions.py

--- 07_Advanced_Functions.py
+++ 07_Advanced_Functions.py
@@ -5,7 +5,7 @@ def multiply_by2(li):
 		new_list.append(item * 2)
 	return new_list
 	
-print(multiply_by2([1,2,3]))
+print(multiply_by2([1,2,3]))		# [2, 4, 6]
 
 
 -----
@@ -15,33 +15,33 @@ def multiply_by2(li):
 	new_list = []
 	for item in li:
 		new_list.append(item * 2)
-	return print(new_list)  # this is not pure, has side effect
+	return print(new_list)  # this is not pure and has a side effect
 	
-multiply_by2([1,2,3])
+multiply_by2([1,2,3])		# [2, 4, 6]
 
 
 ----
 
 
-new_list = []  # this is not pure, has side effect
+new_list = []  # this is not pure and has a side effect
 def multiply_by2(li):
 	for item in li:
 		new_list.append(item * 2)
 	return new_list
 	
-new_list = []  # this is not pure, has side effect
+new_list = []  # this is not pure and has a side effect
 	
-multiply_by2([1,2,3])
+multiply_by2([1,2,3])		#
 
 
 ---------------------------------------------------------------------------
 
 
-# map, filter, zip, reduce
+# map
 def multiply_by2(item):
 	return item * 2
 
-print(list(map(multiply_by2, [1,2,3])))	# [2, 4, 6]
+print(list(map(multiply_by2, [1,2,3])))		# [2, 4, 6]
 
 ---
 
@@ -49,20 +49,24 @@ my_list = [1,2,3]
 def multiply_by2(item):
 	return item * 2
 
-print(list(map(multiply_by2, [1,2,3])))	# [2, 4, 6]
+print(list(map(multiply_by2, my_list)))	# [2, 4, 6]
 print(my_list)	# [1,2,3]
 
----
+
+----------------------------------------
+
 
 # filter
 my_list = [1,2,3]
 def only_odd(item):
     return item % 2 != 0
 
-print(list(filter(only_odd, [1,2,3])))	# [2, 4, 6]
+print(list(filter(only_odd, my_list)))	# [1, 3]
 print(my_list)	# [1,2,3]
 
----
+
+----------------------------------------
+
 
 # zip
 my_list = [1,2,3]
@@ -70,7 +74,7 @@ your_list = [10, 20, 30]
 
 print(list(zip(my_list, your_list)))	# [(1, 10), (2, 20), (3, 30)]
 
---
+---
 
 my_list = [1,2,3]
 your_list = [10, 20, 30]
