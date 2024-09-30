67803fe0ba15d314ca859421e349d291bcdf535a
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2024-09-30 14:11:49 -0700

    Update 09__Advanced_Error_Handling.py

--- 09__Advanced_Error_Handling.py
+++ 09__Advanced_Error_Handling.py
@@ -1,1 +1,53 @@
+### ERROR HANDLING
+
+while True:
+	try: 
+		age = int(input('What is your age?'))
+		print(age)
+	except ValueError:
+		print('Please enter a number')
+	except ZeroDivisionError:
+		print('Please enter age higher than 0')
+	else: 
+		print('thank you')
+		break
+
+
+---
+
+
+def sum(num1, num2):
+	try:
+	    return num1 + num2
+	except TypeError as err:
+		print(f'Please enter numbers --> {err}')
+
+print(sum('1', 2))
+
+
+---
+
+
+def sum(num1, num2):
+	try:
+	    return num1 / num2
+	except (TypeError, ZeroDivisionError):
+		print('Oops')
+
+print(sum(1, 0))
+
+
+---
+
+def sum(num1, num2):
+	try:
+	    return num1 / num2
+	except (TypeError, ZeroDivisionError) as err:
+		print(err)
+
+print(sum(1, 0))
+
+
+
+
 
