5934025e075f1f4cd4be7a4865767d23f492f9ab
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2024-10-01 11:26:26 -0700

    Update 10__Advanced_Generators.py

--- 10__Advanced_Generators.py
+++ 10__Advanced_Generators.py
@@ -13,6 +13,58 @@ my_list = make_list(100)		# my_list is point to location in the memory
 print(my_list)
 
 
+-------------------------------------------------------------------------------------------------------------------
+
+
+# generators are iterable
+# range is a generator
+# list is iterable but not a generator
+
+# general way to create a generator
+# generators are usually functions
+
+def generator_function():
+	for i in range(10000):
+		yield i		# that's the difference, no 'return'
+
 ---
+
+def generator_function(num):
+	for i in range(num):
+		yield i		# yield pauses the functions and comes back when we do smth to it, which is called 'next'
+
+for item in generator_function(100):
+	print(item)
+
+# only held one item in memory instead of having a whole list in memory
+
+
+
+-------------------------------------------------------------------------------------------------------------------
+
+
+
+def generator_function(num):
+	for i in range(num):
+		yield i
+
+g = generator_function(100)	# instead of using a loop
+next(g)
+next(g)
+print(next(g))               # 2
+print(next(g))              # 3
+
+# keep only the most recent data in memory, in this case it is '4'
+
+-----
+
+def generator_function(num):
+	for i in range(num):
+		yield i
+
+g = generator_function(1)	# instead of using a loop
+next(g)
+print(next(g))               # StopIteration - when exeed the number of items
+
 
 
