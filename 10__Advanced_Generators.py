86ebb7f1477a3ba798687e92a985857ac347ecf2
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2024-10-01 19:26:15 -0700

    Update 10__Advanced_Generators.py

--- 10__Advanced_Generators.py
+++ 10__Advanced_Generators.py
@@ -68,3 +68,78 @@ print(next(g))               # StopIteration - when exeed the number of items
 
 
 
+-------------------------------------------------------------------------------------------------------------------
+
+
+#generators.
+#generators are much more performant than lists. (i.e range > list in performance.)
+#So generators are really, really useful when calculating large sets of data.
+
+from time import time
+
+def performance(fn):
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        result = fn(*args, *kwargs)
+        t2 = time()
+        print(f'took {t2-t1} s')
+        return result
+    return wrapper
+
+@performance
+def long_time():
+    print('1')
+    for i in range(1000000): #it finishes after.
+        i*5
+
+long_time()
+print()
+
+@performance
+def long_time2():
+    print('2')
+    for i in list(range(1000000)): #it took longer.
+        i*5
+
+long_time2()
+print()
+
+
+-------------------------------------------------------------------------------------------------------------------
+
+### UNDER HOOD OF GENERATORS
+
+# for - underneath the hood
+def special_for(iterable):
+  iterator = iter(iterable)
+  while True:
+    try:
+      iterator*5
+      next(iterator)
+    except StopIteration:
+      break
+
+
+# range - underneath the hood
+class MyGen:
+  current = 0
+  def __init__(self, first, last):
+    self.first = first
+    self.last = last
+    MyGen.current = self.first #this line allows us to use the current number as the starting point for the iteration
+
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    if MyGen.current < self.last:
+      num = MyGen.current
+      MyGen.current += 1
+      return num
+    raise StopIteration
+
+gen = MyGen(1,100)
+for i in gen:
+    print(i)
+
+
