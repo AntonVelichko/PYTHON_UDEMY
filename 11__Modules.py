e401d4221114c12f3f35bf21202412063b090bee
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2024-10-10 20:37:52 -0700

    Create 11__Modules.py

---
+++ 11__Modules.py
@@ -0,0 +1,22 @@
+## main.py
+import utility
+
+print(utility)
+
+
+## utility.py
+def multiply(num1, num2)
+	return num1 * num2
+
+def devide(num1, num2)
+	return num1 / num2
+
+
+
+
+ ------------------------------------------------------------------------
+
+
+ # IMPORTING MODULES
+
+ 
