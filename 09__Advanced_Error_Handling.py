82b26eca891f05926c17c0c8f5232f91d000f9c0
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2025-01-27 11:33:04 -0800

    Update 09__Advanced_Error_Handling.py

--- 09__Advanced_Error_Handling.py
+++ 09__Advanced_Error_Handling.py
@@ -1,31 +1,54 @@
-### ERROR HANDLING
 
-while True:
+##########################################################################################################################
+###  ERROR HANDLING  ###
+# An error that crashes the program is called an Exception
+# Built-in exceptions  -->  https://docs.python.org/3/library/exceptions.html
+
+
+
+try: 
+	age = int(input('What is your age?'))
+	print(age)						# if enter is not an Integer
+except:
+	print('Please enter a number')
+
+
+
+---------------
+
+
+
+while True:							# allows to start the program for every try
 	try: 
 		age = int(input('What is your age?'))
-		print(age)
-	except ValueError:
+		10/age
+	except ValueError:					# only one of the errors will be caught
 		print('Please enter a number')
 	except ZeroDivisionError:
 		print('Please enter age higher than 0')
 	else: 
 		print('thank you')
-		break
+		break						# finish the program when input is correct
 
 
----
+
+---------------
+
 
 
 def sum(num1, num2):
 	try:
 	    return num1 + num2
 	except TypeError as err:
-		print(f'Please enter numbers --> {err}')
+		print(f'Please enter numbers --> {err}')	# Please enter numbers --> can only concatenate str (not "int") to str
+		print(err)					# can only concatenate str (not "int") to str
 
-print(sum('1', 2))
+print(sum('1', 2))						# None
+								
 
 
----
+---------------
+
 
 
 def sum(num1, num2):
@@ -34,22 +57,41 @@ def sum(num1, num2):
 	except (TypeError, ZeroDivisionError):
 		print('Oops')
 
-print(sum(1, 0))
+print(sum(1, 0))						# Oops
 
 
----
+
+---------------
+
+
 
 def sum(num1, num2):
 	try:
 	    return num1 / num2
 	except (TypeError, ZeroDivisionError) as err:
-		print(err)
+		print(err)					# division by zero
 
-print(sum(1, 0))
+print(sum(1, 0))						# None
 
 
 
----
+---------------
+
+
+
+def sum(num1, num2):
+    try:
+        return num1 / num2
+    except (TypeError, ZeroDivisionError) as err:
+        print(err)						# unsupported operand type(s) for /: 'int' and 'str'
+
+print(sum(1, '2'))						# None
+
+
+
+---------------
+
+
 
 # finally
 while True:
@@ -65,13 +107,18 @@ while True:
 		print('Accepted! Thank you')
 		break
 	finally:
-		print('your attempt is logged')
+		print('your attempt is recorded')				# finally runs regardless of everything
 	print('Can you hear me?')
 
 
----
+
+---------------
+
+
 
 # raise ValueError
+# use very rare, in some specific cases
+
 while True:
 	try: 
 		age = int(input('What is your age?'))
@@ -87,9 +134,13 @@ while True:
 	print('Can you hear me?')
 
 
----
+
+---------------
+
+
 
 # raise Exception
+
 while True:
 	try: 
 		age = int(input('What is your age?'))
