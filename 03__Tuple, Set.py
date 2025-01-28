8d1298ed4703b737d5b5c49ceeb2c8fde1d34db4
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2025-01-28 10:18:29 -0800

    Update and rename 03__55-58 Tuple_Set.py to 03__Tuple, Set.py

--- 03__Tuple, Set.py
+++ 03__Tuple, Set.py
@@ -1,30 +1,39 @@
 
-# 55 Tuple
+##########################################################################################################################
+### TUPLE  ###
 # immutable list (can't be sorted)
+# usually faster then list
+
+
 tuple_1 = (1,2,3,4,5)
-#tuple_1[0] = 10                    # Error
+tuple_1[0] = 10                     # Error
 print(tuple_1[0])                   # 1
+
 
 # in
 print(5 in tuple_1)                 # True
 
+
+# items return tuple
 dict_1 = {
   'a': 1,
   'b': 2
 }
 
-# items return tuple
 print(dict_1.items())               # dict_items([('a', 1), ('b', 2)])
 
 
 
-----------------------------------------------------------------------------------
 
 
 
-#56 Tuple Methods
+##########################################################################################################################
+### TUPLE METHODS  ###
 # https://www.w3schools.com/python/python_ref_tuple.asp
+
+
 tuple_1 = (1,2,3,4,5)
+
 
 # copy
 tuple_2 = tuple_1[1:2]
@@ -34,55 +43,75 @@ print(tuple_1)              # (1, 2, 3, 4, 5)
 tuple_3 = tuple_1[1:4]
 print(tuple_3)              # (2, 3, 4)
 
-x,y,z, *other = (1,2,3,4,5)
+
+
+# can be assign
+x = tuple_1 = [0]                   # [0]
+y = tuple_1 = [1]                   # [1]
+
+x,y,z, *other = (1,2,3,4,5)         # faster way to assign multiple variables
 print(x)                    # 1
 print(other)                # [4, 5]
+
+
 
 # .count   /how many times does item appear in tuple
 tuple_1 = (1,2,3,4,5,2)
 print(tuple_1.count(2))     # 2
 
+
+
 # .index()   /shows the first index of the item if there are more than one similar item
 print(tuple_1.index(5))     # 4
 print(tuple_1.index(2))     # 1
+
+
 
 # len
 print(len(tuple_1))         # 6
 
 
 
-------------------------------------------
 
 
 
-# 57 Sets
+##########################################################################################################################
+### SET  ###
 # unordered collection of unique subjects
+
+
 set_1 = {1,2,3,4,5}
 print(set_1)                                      # {1, 2, 3, 4, 5}
 
 set_2 = {1,2,3,4,5,5}
 print(set_2)                                      # {1, 2, 3, 4, 5}
 
+
 # .add()
 set_2.add(6)
-set_2.add(2)
+set_2.add(2)                                      # doesn't add '2'
 print(set_2)                                      # {1, 2, 3, 4, 5, 6}
+
 
 # set()   /convert list to set
 list_1 = [1,2,3,4,5,5,4]
 print(set(list_1))                                # {1, 2, 3, 4, 5}
 
+
 # set like dictionary can't be addressed by index, only by value
 print(1 in set_2)                                 # True
-#print(set_2[1])                                  # Error
-#print(set_2[0])                                  # Error
+print(set_2[1])                                  # Error
+print(set_2[0])                                  # Error
+
 
 # len()   /count only unique values
 set_3 = {1,2,3,3,2,1}
 print(len(set_3))                                 # 3
 
+
 # list()   /convert set to list
 print(list(set_3))                                # [1,2,3]
+
 
 # .copy()
 set_4 = set_3.copy()
@@ -90,20 +119,23 @@ set_4.add('a')
 print(set_4)                                      # {1, 2, 3, 'a'}
 print(set_3)                                      # {1, 2, 3}
 
+
 # .clear()
 set_4.clear()
 print(set_4)                                      # set()
 
 
 
-------------------------------------------
 
 
 
-# 58 Set Methods
+##########################################################################################################################
+### SET METHODS  ###
 # https://www.w3schools.com/python/python_ref_set.asp
+
 set_1 = {1,2,3,4,5}
 set_2 = {4,5,6,7,8,9,10}
+
 
 # .difference()   / find difference / returns difference
 set_1 = {1,2,3,4,5}
@@ -111,11 +143,14 @@ print(set_1.difference(set_2))                          # {1,2,3}
 print(set_2.difference(set_1))                          # {6, 7, 8, 9, 10}
 print(set_1)                                            # {1, 2, 3, 4, 5}
 
+
+
 # .discard()   /delete item / returns None
 set_1 = {1,2,3,4,5}
 set_2 = {4,5,6,7,8,9,10}
 set_1.discard(5)
 print(set_1)                                            # {1, 2, 3, 4}
+
 
 # .difference_update()   /returns None
 # removes all elements of another set from this set
@@ -123,6 +158,8 @@ set_1 = {1,2,3,4,5}
 set_2 = {4,5,6,7,8,9,10}
 set_1.difference_update(set_2)
 print(set_1)                                            # {1,2,3}
+
+
 
 # .intersection()
 set_1 = {1,2,3}
@@ -140,10 +177,14 @@ print(set_1.intersection(set_2))                        # {9,10}
 print(set_1 & set_2)                                    # {9, 10}
 print(set_1)                                            # {9,10,11}
 
+
+
 # .isdisjoint()   /if it hasn't common values
 set_1 = {1,2,3}
 set_2 = {4,5,6,7,8,9,10}
 print(set_1.isdisjoint(set_2))                          # True
+
+
 
 # .union()
 set_1 = {1,2,3,4,5}
@@ -153,6 +194,8 @@ print(set_1.union(set_2))                               # {1, 2, 3, 4, 5, 6, 7, 
 set_1 = {3,4,5}
 print(set_1 | set_2)                                    # {3, 4, 5, 6, 7, 8, 9, 10}
 
+
+
 # .issubset()   / returns whether another set contains this set or not
 set_1 = {4,5}
 set_2 = {4,5,6,7,8,9,10}
@@ -161,6 +204,8 @@ print(set_1.issubset(set_2))                            # True
 set_1 = {3,4,5}
 set_2 = {4,5,6,7,8,9,10}
 print(set_1.issubset(set_2))                            # False
+
+
 
 # .issuperset()   / returns whether this set contains another set or not
 set_1 = {4,5}
@@ -174,19 +219,28 @@ print(set_2.issuperset(set_1))                          # False
 
 
 
-------------------------------------------
 
 
+
+*************************************************************************************************************************
+***  EXERCISE  ***
 
 # You are working for the school Principal. We have a database of school students:
 school = {'Bobby','Tammy','Jammy','Sally','Danny'}
 
-#during class, the teachers take attendance and compile it into a list. 
+# during class, the teachers take attendance and compile it into a list. 
 attendance_list = ['Jammy', 'Bobby', 'Danny', 'Sally']
 
-#using what you learned about sets, create a piece of code that the school principal 
-#can use to immediately find out who missed class so they can call the parents. 
-#(Imagine if the list had 1000s of students. The principal can use the lists generated by 
-#the teachers + the school database to use python and make his/her job easier): Find the students that miss class!
+# using what you learned about sets, create a piece of code that the school principal 
+# can use to immediately find out who missed class so they can call the parents. 
+# (Imagine if the list had 1000s of students. The principal can use the lists generated by 
+# the teachers + the school database to use python and make his/her job easier): Find the students that miss class!
 
 print(school.difference(attendance_list))    # we don't have to convert the attendance_list to a set...it does it for you
+
+
+
+
+
+
+-
