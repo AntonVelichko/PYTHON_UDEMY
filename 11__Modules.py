97f1bec0c658398e0ea37133a632a53630eec4cf
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2025-01-29 11:30:28 -0800

    Update 11__Modules.py

--- 11__Modules.py
+++ 11__Modules.py
@@ -1,22 +1,2 @@
-## main.py
-import utility
-
-print(utility)
-
-
-## utility.py
-def multiply(num1, num2)
-	return num1 * num2
-
-def devide(num1, num2)
-	return num1 / num2
-
-
-
-
- ------------------------------------------------------------------------
-
-
- # IMPORTING MODULES
 
  
