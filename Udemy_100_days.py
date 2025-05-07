d0e9cccb3ed6acc35cb545e5ea4462f81f2d10d5
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2025-05-06 22:22:17 -0400

    Update Udemy_100_days.py

--- Udemy_100_days.py
+++ Udemy_100_days.py
@@ -48,4 +48,32 @@ while not at_goal():
 
 
 
+##########################################################################################################################
+###  DAY 8  ###
+
+
+def calculate_love_score(name1, name2):
+    true_check = "true"
+    love_check = "love"   
+    
+    true_score = 0
+    love_score = 0
+    
+    for i in range(0, len(name1)):
+        if name1[i].lower() in true_check:
+            true_score += 1
+        if name1[i].lower() in love_check:
+            love_score += 1
+            
+    for i in range(0, len(name2)):
+        if name2[i].lower() in true_check:
+            true_score += 1
+        if name2[i].lower() in love_check:
+            love_score += 1   
+            
+    return str(true_score) + str(love_score)
+    
+
+print(calculate_love_score("Angela Yu","Jack Bauer"))
+print(calculate_love_score("Kanye West", "Kim Kardashian"))
 
