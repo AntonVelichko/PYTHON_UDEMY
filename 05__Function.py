8712c54ba8568b1220802c09e14a254edc64c339
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2025-05-17 21:03:43 -0400

    Update 05__Function.py

--- 05__Function.py
+++ 05__Function.py
@@ -376,4 +376,23 @@ def hanoi(n, a, b, c):
 # решение задачи для трёх колец
 hanoi(3, 'A', 'B', 'C')
 
+
+
+
+##########################################################################################################################
+###  INPUT WITH FUNCTION  ###
+
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You did not provide valid inputs"
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"Result: {formated_f_name} {formated_l_name}"
+
+
+print(format_name(input("What is your first name?"), input("What is your last name?")))
+
+
+
+
 .
