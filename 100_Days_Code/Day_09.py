49f5c29413bc519524f4a38608190275ce36a0a6
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2025-05-17 22:17:21 -0400

    Update Day_09.py

--- 100_Days_Code/Day_09.py
+++ 100_Days_Code/Day_09.py
@@ -1,3 +1,53 @@
 
 ##########################################################################################################################
 ###  exercise Grading  ###
+
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+
+for key in student_scores:
+    if student_scores[key] > 90:
+        student_grades[key] = "Outstanding" 
+    elif student_scores[key] > 80:
+        student_grades[key] = "Exceeds Expectations"
+    elif student_scores[key] > 70:
+        student_grades[key] = "Acceptable"
+    else:
+        student_grades[key] = "Fail"
+        
+print(student_grades)
+
+---
+
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+ 
+def score_to_grade(score):
+    if score > 90:
+        return "Outstanding"
+    elif score > 80:
+        return "Exceeds Expectations"
+    elif score > 70:
+        return "Acceptable"
+    else:
+        return "Fail"
+ 
+student_grades = {}
+ 
+for key in student_scores:
+    student_grades[key] = score_to_grade(student_scores[key])
+
+
+.
