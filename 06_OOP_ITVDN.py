3d7ec69468b5c0f642d9bc43e40d1d5ffc481e22
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2025-05-27 21:54:15 -0400

    Update 06_OOP_ITVDN.py

--- 06_OOP_ITVDN.py
+++ 06_OOP_ITVDN.py
@@ -459,6 +459,172 @@ Every time you call MyObject(), it creates a new object, so changes made to one 
 
 
 
+##########################################################################################################################
+###  MAGIC/SPECIAL METHODS  ###
+
+# Атрибуты, имена которых начинаются и заканчиваются двумя знаками подчёркивания, являются внутренними для Python и задают особые
+# свойства объектов. С одним из подобных атрибутов мы уже имели дело ранее (документационная строка __doc__). Другим примером может служить атрибут __class__, в котором хранится класс
+# данного объекта.
+#
+# Среди таких атрибутов есть методы. В документации Python подобные методы называются методами со специальными именами,
+# однако в сообществе Python-разработчиков очень распространено название «магические методы». Также встречается и название
+# «специальные методы». Они задают особое поведение объектов и позволяют переопределять поведение встроенных функций и
+# операторов для экземпляров данного класса.
+#
+# Наиболее часто используемым специальным методом является метод-конструктор __init__.
+
+
+class Complex:
+    """
+    Комплексное число
+    """
+
+    def __init__(self, real=0.0, imaginary=0.0):
+        """
+        Конструктор
+
+        :param real:      действительная часть
+        :param imaginary: мнимая часть
+        """
+        self.real = real
+        self.imaginary = imaginary
+
+    def __repr__(self):
+        """
+        Метод __repr__ возвращает строковое представление объекта, которое,
+        если это возможно, должно быть корректным выражением, создающим
+        аналогичный объект, иначе содержать его описание;
+        вызывается функцией repr.
+        """
+        return 'Complex(%g, %g)' % (self.real, self.imaginary)
+
+    def __str__(self):
+        """
+        Метод __str__ возвращает предназначенное для человека строковое
+        представление объекта; вызывается функциями str, print и format.
+        """
+        return '%g %c %gi' % (self.real,
+                              '+' if self.imaginary >= 0 else '-',
+                              abs(self.imaginary))
+
+    # Арифметические операции
+
+    def __add__(self, other):
+        """
+        Метод __add__ определяет операцию сложения.
+        """
+        return Complex(self.real + other.real,
+                       self.imaginary + other.imaginary)
+
+    def __neg__(self):
+        """
+        Операция отрицания
+        """
+        return Complex(-self.real, -self.imaginary)
+
+    def __sub__(self, other):
+        """
+        Операция вычитания.
+        Сложение и отрицание уже определены, поэтому вычитание
+        можно определить через них
+        """
+        return self + (-other)
+
+    def __abs__(self):
+        """
+        Модуль числа
+        """
+        return (self.real ** 2 + self.imaginary ** 2) ** 0.5
+
+    # Операции сравнения
+
+    def __eq__(self, other):
+        return self.real == other.real and self.imaginary == other.imaginary
+
+    def __ne__(self, other):
+        return not (self == other)
+
+
+def main():
+    x = Complex(2, 3.5)
+    print(repr(x))                          # Complex(2, 3.5)
+    print('x =', x)                         # x = 2 + 3.5i
+
+    y = Complex(5, 7)
+    print('y =', y)                         # y = 5 + 7i
+
+    print('x + y =', x + y)                 # x + y = 7 + 10.5i
+    print('x - y =', x - y)                 # x - y = -3 - 3.5i
+
+    print('|x| =', abs(x))                  # |x| = 4.031128874149275
+
+    print('(x == y) =', x == y)             # (x == y) = False
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+x = 2
+print(x.__neg__())                          # -2
+
+
+
+class Custom:
+    def __init__(self, num):
+        self.num = num
+    
+    def __eq__(self, other):
+        # return self.num == other.num
+        return 'Works!'
+
+
+num1 = Custom(1)
+num2 = Custom(1)
+print(num1.__eq__(num2))                    # Works!
+
+
+
+
+
+
+
+##########################################################################################################################
+###  SECRET DATA  ###
+# В этом примере показано использование функции __getattribute__ для сокрытия данных
+
+class MyClass:
+    def __init__(self):
+        self.password = None
+
+    def __getattribute__(self, item):
+        """
+        Метод __getattribute__ вызывается при получении атрибутов
+        """
+
+        # Если запрошено поле secret_field и пароль верный
+        if item == 'secret_field' and self.password == '9ea)fc':
+            # то возвращаем значение
+            return 'secret value'
+        else:
+            # иначе вызываем метод __getattribute__ класса object
+            return object.__getattribute__(self, item)
+
+
+# Создание экземпляра класса
+obj = MyClass()
+
+# Разблокирование секретного поля
+obj.password = '9ea)fc'             # comment it ro see Error
+
+# Вывод значения secret field.
+# Значение будет получено, если раскомментировать предыдущую строку программного кода, иначе будет получена ошибка.
+print(obj.secret_field)             # secret value
+
+
+
+
 
 
 ..[end]
