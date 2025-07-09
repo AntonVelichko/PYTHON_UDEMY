13e01604117a9d4c7569ae0b12d73c35ec5dd6e1
AntonVelichko <65468861+AntonVelichko@users.noreply.github.com>
2025-07-08 22:58:46 -0700

    Update Day_02.py

--- 100_Days_Code/Day_02.py
+++ 100_Days_Code/Day_02.py
@@ -1,1 +1,30 @@
+##########################################################################################################################
+###  TIP CALCULATOR PROJECT  ###
+
+
+............................................................................................
+../my_solution.py
+  
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+splitted_bill = round(((bill / people) * (tip+100)/100), 2)
+print(f"Each person should pay: ${splitted_bill}")
+
+
+
+............................................................................................
+../teacher_solution.py
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+print(f"Each person should pay: ${final_amount}")
 
